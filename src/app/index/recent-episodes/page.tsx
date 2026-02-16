74d503719aa1484e4a5ee6f5e956aa3c7fb3e92a
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-17 00:40:19 +0530

    feat: impl  recent episodes v2

--- src/app/index/recent-episodes/page.tsx
+++ src/app/index/recent-episodes/page.tsx
@@ -110,6 +110,78 @@ const RecentEpisodes = () => {
                   <div className="music-waves2"></div>
                 </div>
               </div>
+
+              <h2 className="text-gray-300 text-2xl md:text-3xl font-semibold">
+                Laughter Unleashed: Join Us for Joyful Conversations Today
+              </h2>
+              <p className=" my-4 tracking-wide">
+                Embark on a journey of unexplored wisdom as we delve into
+                fascinating topics that challenge the mind
+              </p>
+              <div className="flex justify-between items-center gap-5 ">
+                <Link
+                  href="/app/pages"
+                  className="flex items-center gap-2 group"
+                >
+                  <i
+                    className="bi bi-play p-2 bg-primary rounded-full flex items-center 
+                    justify-center text-black text-2xl group-hover:bg-secondary 
+                    group-hover:text-white cursor-pointer transition-all duration-200"
+                  ></i>
+                  <h2 className="text-xl underline text-black group-hover:text-secondary transition-all duration-200">
+                    Listen Now
+                  </h2>
+                </Link>
+                <span className="bg-gray-800 text-primary px-5 py-2 rounded-full text-xl">
+                  Episode 04
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full lg:w-1/2">
+            <div className="p-6 rounded-2xl bg-gray">
+              <div className="flex flex-wrap items-center gap-5">
+                <Link href="/app/pages">
+                  <h2 className="text-gray-300">
+                    <i className="bi bi-mic"> Devon Lane</i>
+                  </h2>
+                </Link>
+
+                <h2 className="text-gray-300">
+                  <i className="bi bi-clock pe-1"></i> 4hr 12min
+                </h2>
+                <div className="flex items-center gap-3">
+                  <i className="bi bi-balloon-heart me-3 text-gray-300 text-xl"></i>
+                  <div className="music-waves2"></div>
+                </div>
+              </div>
+
+              <h2 className="text-gray-300 text-2xl md:text-3xl font-semibold">
+                Behind the Scenes: Candid Talks on Podcasting Strategies
+              </h2>
+              <p className=" my-4 tracking-wide">
+                Unlock the secrets of inspiration with heartfelt stories and
+                motivational insights in this uplifting episode
+              </p>
+              <div className="flex justify-between items-center gap-5 ">
+                <Link
+                  href="/app/pages"
+                  className="flex items-center gap-2 group"
+                >
+                  <i
+                    className="bi bi-play p-2 bg-primary rounded-full flex items-center 
+                    justify-center text-black text-2xl group-hover:bg-secondary 
+                    group-hover:text-white cursor-pointer transition-all duration-200"
+                  ></i>
+                  <h2 className="text-xl underline text-black group-hover:text-secondary transition-all duration-200">
+                    Listen Now
+                  </h2>
+                </Link>
+                <span className="bg-gray-800 text-primary px-5 py-2 rounded-full text-xl">
+                  Episode 04
+                </span>
+              </div>
             </div>
           </div>
         </div>
