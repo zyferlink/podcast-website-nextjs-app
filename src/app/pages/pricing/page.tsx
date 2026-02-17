91b839f3109d6ed649c97f4ae16e149d13a2258e
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-17 22:36:30 +0530

    feat (func-ui) : pricing page - impl v1

--- src/app/pages/pricing/page.tsx
+++ src/app/pages/pricing/page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
 
-const Page = () => {
-  return <div>Page</div>;
+import type { Swiper as SwiperType } from "swiper";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import HostImage1 from "../../../../public/images/host-profile-1.png";
+import HostImage2 from "../../../../public/images/host-profile-2.png";
+import HostImage3 from "../../../../public/images/host-profile-3.png";
+import HostImage4 from "../../../../public/images/host-profile-4.png";
+import HostImage5 from "../../../../public/images/host-profile-5.png";
+import HostImage6 from "../../../../public/images/host-profile-6.png";
+import HostImage7 from "../../../../public/images/host-profile-7.png";
+import HostImage8 from "../../../../public/images/host-profile-8.png";
+import HostImage9 from "../../../../public/images/host-profile-9.png";
+import HostImage10 from "../../../../public/images/host-profile-10.png";
+import "swiper/css";
+import Image from "next/image";
+import Link from "next/link";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
+import BrandIcon1 from "../../../../public/images/brand-icon-1.png";
+import BrandIcon2 from "../../../../public/images/brand-icon-2.png";
+import BrandIcon3 from "../../../../public/images/brand-icon-3.png";
+import BrandIcon4 from "../../../../public/images/brand-icon-4.png";
+import BrandIcon5 from "../../../../public/images/brand-icon-5.png";
+import PageBanner1 from "../../../../public/images/Page-banner-1.png";
+import PageBanner2 from "../../../../public/images/Page-banner-2.png";
+import PageElement1 from "../../../../public/images/page-elm-1.png";
+import PageElement2 from "../../../../public/images/page-elm-2.png";
+import PageElement3 from "../../../../public/images/page-elm-3.png";
+import PageElement4 from "../../../../public/images/page-elm-4.png";
+import RocketIcon from "../../../../public/images/rocket-icon.png";
+import EpisodesData from "../../../data/EpisodeData.json";
+
+const Pricing = () => {
+  return <div>Pricing</div>;
 };
 
-export default Page;
+export default Pricing;
