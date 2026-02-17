3506a5c98ce9cf2323e1894d4a44a030720cb631
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-17 21:34:41 +0530

    feat (func) : host profile page - impl v1

--- src/app/pages/host-profile/page.tsx
+++ src/app/pages/host-profile/page.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+"use client";
 
-const Page = () => {
-  return <div>Page</div>;
+import type { Swiper as SwiperType } from "swiper";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import HostImage1 from "../../../../public/images/host-profile-1.png";
+import HostImage2 from "../../../../public/images/host-profile-2.png";
+import HostImage3 from "../../../../public/images/host-profile-3.png";
+import HostImage4 from "../../../../public/images/host-profile-4.png";
+import HostImage5 from "../../../../public/images/host-profile-5.png";
+import HostImage6 from "../../../../public/images/host-profile-6.png";
+import HostImage7 from "../../../../public/images/host-profile-7.png";
+import HostImage8 from "../../../../public/images/host-profile-8.png";
+import HostImage9 from "../../../../public/images/host-profile-9.png";
+import HostImage10 from "../../../../public/images/host-profile-10.png";
+import "swiper/css";
+import Image from "next/image";
+import Link from "next/link";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
+import BrandIcon1 from "../../../../public/images/brand-icon-1.png";
+import BrandIcon2 from "../../../../public/images/brand-icon-2.png";
+import BrandIcon3 from "../../../../public/images/brand-icon-3.png";
+import BrandIcon4 from "../../../../public/images/brand-icon-4.png";
+import BrandIcon5 from "../../../../public/images/brand-icon-5.png";
+import PageBanner1 from "../../../../public/images/Page-banner-1.png";
+import PageBanner2 from "../../../../public/images/Page-banner-2.png";
+import PageElement1 from "../../../../public/images/page-elm-1.png";
+import PageElement2 from "../../../../public/images/page-elm-2.png";
+import PageElement3 from "../../../../public/images/page-elm-3.png";
+import PageElement4 from "../../../../public/images/page-elm-4.png";
+import RocketIcon from "../../../../public/images/rocket-icon.png";
+import EpisodesData from "../../../data/EpisodeData.json";
+
+const HostProfile = () => {
+  return (
+    <>
+      {/* Page Section */}
+      <div className="page-section bg-black/98 text-white">
+        <Image src={PageElement2} alt="Element" className="elm2 elemet" />
+        <Image src={PageElement3} alt="Element" className="elm3 elemet" />
+        <Image src={PageElement4} alt="Element" className="elm4 elemet" />
+        <div className="page-content w-full md:w-1/2 flex justify-center flex-col pt-10">
+          <Image src={PageElement1} alt="Element" className="w-full h-full" />
+
+          <h1 className="text-6xl lg:text-8x1 justify-center my-6">
+            Host <span className="text-primary">Profile</span>
+          </h1>
+
+          <div className="flex items-center justify-center gap-3 cursor-pointer mt-5">
+            <Image src={BrandIcon1} alt="brand" />
+            <Image src={BrandIcon2} alt="brand" />
+            <Image src={BrandIcon3} alt="brand" />
+            <Image src={BrandIcon4} alt="brand" />
+            <Image src={BrandIcon5} alt="brand" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Page;
+export default HostProfile;
