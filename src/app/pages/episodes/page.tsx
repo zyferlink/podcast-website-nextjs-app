76354bea5544e36825a4e89dafc061508fb434a0
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-17 14:10:54 +0530

    feat: section episodes : serach bar

--- src/app/pages/episodes/page.tsx
+++ src/app/pages/episodes/page.tsx
@@ -15,7 +15,7 @@ const Episodes = () => {
   return (
     <>
       {/* Page Section */}
-      <div className="page-section bg-black/95 text-white">
+      <div className="page-section bg-black/98 text-white">
         <Image src={PageElement2} alt="Element" className="elm2 elemet" />
         <Image src={PageElement3} alt="Element" className="elm3 elemet" />
         <Image src={PageElement4} alt="Element" className="elm4 elemet" />
@@ -32,6 +32,46 @@ const Episodes = () => {
             <Image src={BrandIcon3} alt="brand" />
             <Image src={BrandIcon4} alt="brand" />
             <Image src={BrandIcon5} alt="brand" />
+          </div>
+        </div>
+      </div>
+
+      {/* Episodes */}
+      <div className="dark-section  bg-black/98 text-white">
+        <div className="px-[8%] lg:px-[16%] py-30 pb-0 md:pb-10 relative">
+          {/* Search Episodes */}
+          <div className="w-full bg-gray px-5 py-3 rounded-full flex justify-between items-center gap-5 episode-search">
+            <input
+              type="text"
+              placeholder="Search Episode ... "
+              // value={(e) => }
+              className="w-[80%] py-3 outline-none ps-5 text-xl bg-transparent"
+            />
+            <button type="button" className="btn btn2">
+              Search <i className="bi bi-arrow-right-short"></i>
+            </button>
+          </div>
+
+          <div className="flex justify-between items-center gap-5">
+            <h2>Total Episodes Available (5 )</h2>
+
+            {/* Sorting */}
+            <div className="relative">
+              <select
+                // value={}
+                // value={(e) => }
+                className="appearance-none bg-gray text-primary px-5 py-3 pr-12 
+                rounded-full outline-none cursor-pointer font-medium hover:bg-gray-light 
+                transition-all duration-300"
+              >
+                <option value="default">Sort by</option>
+                <option value="high">Episode: High to Low</option>
+                <option value="low">Episode: Low to High</option>
+                <option value="title">Title: A to Z</option>
+              </select>
+
+              <i className="bi bi-chevron-down absolute right-5 top-1/2 text-primary pointer-events-none"></i>
+            </div>
           </div>
         </div>
       </div>
