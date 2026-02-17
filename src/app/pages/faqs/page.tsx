f3d01a16ff1b783f6bd5b1d2caa3d49c17af9e95
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-18 00:09:29 +0530

    feat (func-ui) : faqs page - impl v1

--- src/app/pages/faqs/page.tsx
+++ src/app/pages/faqs/page.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import BrandIcon1 from "../../../../public/images/brand-icon-1.png";
+import BrandIcon2 from "../../../../public/images/brand-icon-2.png";
+import BrandIcon3 from "../../../../public/images/brand-icon-3.png";
+import BrandIcon4 from "../../../../public/images/brand-icon-4.png";
+import BrandIcon5 from "../../../../public/images/brand-icon-5.png";
+import PageBanner1 from "../../../../public/images/Page-banner-1.png";
+import PageBanner2 from "../../../../public/images/Page-banner-2.png";
+import PageElement1 from "../../../../public/images/page-elm-1.png";
+import PageElement2 from "../../../../public/images/page-elm-2.png";
+import PageElement3 from "../../../../public/images/page-elm-3.png";
+import PageElement4 from "../../../../public/images/page-elm-4.png";
+import RocketIcon from "../../../../public/images/rocket-icon.png";
 
-const Page = () => {
-  return <div>Page</div>;
+const Faqs = () => {
+  return (
+    <>
+      {/* Page Section */}
+      <div className="page-section bg-black/98 text-white">
+        <Image src={PageElement2} alt="Element" className="elm2 elemet" />
+        <Image src={PageElement3} alt="Element" className="elm3 elemet" />
+        <Image src={PageElement4} alt="Element" className="elm4 elemet" />
+        <div className="page-content w-full md:w-1/2 flex justify-center flex-col pt-10">
+          <Image src={PageElement1} alt="Element" className="w-full h-full" />
+
+          <h1 className="text-6xl lg:text-8xl justify-center my-6">
+            FAQ<span className="text-primary">s</span>
+          </h1>
+
+          <div className="flex items-center justify-center gap-3 cursor-pointer mt-5">
+            <Image src={BrandIcon1} alt="brand" />
+            <Image src={BrandIcon2} alt="brand" />
+            <Image src={BrandIcon3} alt="brand" />
+            <Image src={BrandIcon4} alt="brand" />
+            <Image src={BrandIcon5} alt="brand" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Page;
+export default Faqs;
