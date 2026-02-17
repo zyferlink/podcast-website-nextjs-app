67d0b00ceb062ac5768660a26f7c88c15bfabc91
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-17 20:21:16 +0530

    feat (func) : about page - impl v1

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
 
-const Page = () => {
+import Link from "next/link";
+import CountUp from "react-countup";
+import BrandIcon1 from "../../../../public/images/brand-icon-1.png";
+import BrandIcon2 from "../../../../public/images/brand-icon-2.png";
+import BrandIcon3 from "../../../../public/images/brand-icon-3.png";
+import BrandIcon4 from "../../../../public/images/brand-icon-4.png";
+import BrandIcon5 from "../../../../public/images/brand-icon-5.png";
+import ExperienceBanner1 from "../../../../public/images/elevate-banner-1.png";
+import ExperienceBanner2 from "../../../../public/images/elevate-banner-2.png";
+import ExperienceBannerArrow1 from "../../../../public/images/elevate-banner-arrow1.png";
+import ExperienceBannerArrow2 from "../../../../public/images/elevate-banner-arrow2.png";
+import WhyChooseUsImage1 from "../../../../public/images/why-choose-1.png";
+import WhyChooseUsImage2 from "../../../../public/images/why-choose-2.png";
+import WhyChooseUsImage3 from "../../../../public/images/why-choose-3.png";
+
+const About = () => {
   return <div>Page</div>;
 };
 
-export default Page;
+export default About;
