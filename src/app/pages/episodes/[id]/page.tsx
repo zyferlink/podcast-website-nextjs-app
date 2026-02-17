ba0edd9a58778a6561119e9d6965a381a9a2df41
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-17 19:05:32 +0530

    feat (func) : episode details - impl v3

--- src/app/pages/episodes/[id]/page.tsx
+++ src/app/pages/episodes/[id]/page.tsx
@@ -28,6 +28,34 @@ type Episode = {
   image: string;
 };
 
+const faqs = [
+  {
+    question: "How often are new episodes released?",
+    answer:
+      "If you have premium or exclusive content, provide information on how listeners can access it. Share information about any online communities or forums where listeners can connect and discuss episodes.",
+  },
+  {
+    question: "Can I suggest topics or guests for future episodes?",
+    answer:
+      "If you have premium or exclusive content, provide information on how listeners can access it. Share information about any online communities or forums where listeners can connect and discuss episodes.",
+  },
+  {
+    question: "Is there a way to get early access or exclusive content?",
+    answer:
+      "If you have premium or exclusive content, provide information on how listeners can access it. Share information about any online communities or forums where listeners can connect and discuss episodes.",
+  },
+  {
+    question: "How do I leave a review or feedback?",
+    answer:
+      "If you have premium or exclusive content, provide information on how listeners can access it. Share information about any online communities or forums where listeners can connect and discuss episodes.",
+  },
+  {
+    question: "Do you accept listener questions?",
+    answer:
+      "If you have premium or exclusive content, provide information on how listeners can access it. Share information about any online communities or forums where listeners can connect and discuss episodes.",
+  },
+];
+
 const EpisodeDetails = () => {
   const { id } = useParams();
 
