96f535f9aae2976663e6c69852f933c80086d0d9
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-18 12:32:26 +0530

    feat (func-ui) : blog details page - impl v1

--- src/app/pages/blogs/[id]/page.tsx
+++ src/app/pages/blogs/[id]/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+import BlogsData from "../../../../data/BlogsData.json";
+import BrandIcon1 from "../../../../public/images/brand-icon-1.png";
+import BrandIcon2 from "../../../../public/images/brand-icon-2.png";
+import RocketIcon from "../../../../public/images/rocket-icon.png";
 
-const Page = () => {
+const BlogDetails = () => {
   return <div>Page</div>;
 };
 
-export default Page;
+export default BlogDetails;
