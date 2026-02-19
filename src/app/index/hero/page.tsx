b4c5e5888aaae4cfc496d19140096e50262e9ca5
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-19 19:05:02 +0530

    refactor (index::hero): v5 - update hero comp

--- src/app/index/hero/page.tsx
+++ src/app/index/hero/page.tsx
@@ -1,5 +1,9 @@
+"use client";
 // Next.js components
 import Image from "next/image";
+import { FaArrowRight } from "react-icons/fa6";
+import { StyledButtonPrimary } from "@/components/buttons/styled-button-primary";
+import { StyledButtonSecondary } from "@/components/buttons/styled-button-secondary";
 // Assets
 import BrandIcon1 from "../../../../public/images/brand-icon-1.png";
 import BrandIcon2 from "../../../../public/images/brand-icon-2.png";
@@ -20,7 +24,7 @@ const Hero = () => {
       {/* vector line drawing*/}
       <div className="absolute top-0 left-0 w-full h-full bg-[url('/images/vector-line.png')] bg-no-repeat -z-10" />
 
-      {/* Rotating sun elements */}
+      {/* rotating sun elements */}
       <div className="sun-elem">
         <Image src={SunShapeImage} alt="Sun Shape Image" />
         <Image src={SunShapeImage} alt="Sun Shape Image" />
@@ -34,20 +38,28 @@ const Hero = () => {
               <Image src={HeroBarImage} alt="hero bar" className="h-10" />
               <h1 className="text-3xl md:text-4xl my-4 font-bold text-white">
                 Explore <span className="text-primary"> Sound </span>{" "}
-                Storytelling Magic .
+                Storytelling Magic
               </h1>
               <p className="tracking-normal text-base text-white/80">
                 Discover the harmony of heartfelt conversations. Bringing
                 clarity to life's complexities. Focus on what matters most
               </p>
 
-              <div className="hero-btn flex items-center gap-5 my-5">
-                <button type="button" className="btn btn2">
-                  Latest <i className="bi bi-arrow-right-short"></i>
-                </button>
-                <button type="button" className="btn btn1">
-                  Subscribe <i className="bi bi-arrow-right-short"></i>
-                </button>
+              {/* action buttons */}
+              <div className="flex items-center gap-5 my-5">
+                <StyledButtonSecondary
+                  onClick={() => {}}
+                  icon={<FaArrowRight size={12} />}
+                >
+                  Latest
+                </StyledButtonSecondary>
+
+                <StyledButtonPrimary
+                  onClick={() => {}}
+                  icon={<FaArrowRight size={12} />}
+                >
+                  Subscribe
+                </StyledButtonPrimary>
               </div>
 
               <div className="mt-4">
