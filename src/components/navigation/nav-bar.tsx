5eb5c28b5b860c3d16a9acfd4e2a87b700f0f43c
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-19 15:12:50 +0530

    refactor (comp::nav-bar): update nav link data

--- src/components/navigation/nav-bar.tsx
+++ src/components/navigation/nav-bar.tsx
@@ -4,44 +4,20 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { navLinks } from "@/data/navigation";
 import MenuIcon from "../../../public/assets/images/menu-indicator.svg";
 import LogoIcon from "../../../public/assets/images/voice-flow-icon.png";
 
-type NavLink = {
-  label: string;
-  href: string;
-  dropdown?: { label: string; href: string }[];
-};
-
-const navLinks: NavLink[] = [
-  { label: "Home", href: "/" },
-  {
-    label: "Episodes",
-    href: "/pages/episodes",
-    dropdown: [
-      { label: "Latest Episodes", href: "/pages/episodes/latest-episodes" },
-      { label: "Favorite Episodes", href: "/pages/episodes/favorite-episodes" },
-    ],
-  },
-  { label: "Blogs", href: "/pages/blogs" },
-  {
-    label: "More",
-    href: "/pages/about",
-    dropdown: [
-      { label: "About", href: "/pages/about" },
-      { label: "Host Profiles", href: "/pages/host-profile" },
-      { label: "Pricing Plan", href: "/pages/pricing" },
-      { label: "FAQ's", href: "/pages/faqs" },
-      { label: "Contact Us", href: "/pages/contact" },
-    ],
-  },
-];
-
 export default function Navbar() {
+  const pathname = usePathname();
+
   const [open, setOpen] = useState(false);
   const [openDropdown, setOpenDropdown] = useState<string | null>(null);
-
   const [scrolled, setScrolled] = useState(false);
+
+  // Login and registration models
+  const [showModal, setShowModal] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -52,23 +28,16 @@ export default function Navbar() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const pathname = usePathname();
-
   const toggleDropdown = (label: string) => {
     setOpenDropdown((prev) => (prev === label ? null : label));
   };
-
-  // Login and Registration Model
-
-  const [showModal, setShowModal] = useState(false);
-  const [isLogin, setIsLogin] = useState(true);
 
   return (
     <>
       {/* Navbar */}
       <div
         className={`px-[8%] lg:px-[16%] fixed top-0 left-0 w-full z-50 navbar transition-all duration-300 
-            ${scrolled ? "bg-black/95 backdrop-blur-md shadow-lg" : "bg-transparent "} `}
+            ${scrolled ? "bg-black/80 backdrop-blur-md shadow-lg" : "bg-transparent "} `}
       >
         <div className="flex justify-between items-center gap-5 py-3">
           <div className="flex items-center gap-8">
