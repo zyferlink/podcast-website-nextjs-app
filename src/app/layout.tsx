c77617a4a03fea17cf4ac8f89c00282c33566866
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-19 15:38:20 +0530

    refactor (layout): update fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Kanit, Metal_Mania } from "next/font/google";
+import { Geist, Geist_Mono, Poiret_One, Zain } from "next/font/google";
 import "./globals.css";
 
-const kanit = Kanit({
-  weight: ["200", "300", "400", "500", "600", "700"],
-  variable: "--font-kanit",
+const fontZain = Zain({
+  weight: ["200", "300", "400", "700", "800", "900"],
+  variable: "--font-zain",
   subsets: ["latin"],
 });
 
-const metalMania = Metal_Mania({
+const fontPoiretOne = Poiret_One({
   weight: ["400"],
-  variable: "--font-metal-mania",
+  variable: "--font-poiret-one",
   subsets: ["latin"],
 });
 
-const geistSans = Geist({
+const fontGeistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const fontGeistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -39,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${kanit.variable} ${metalMania.variable} antialiased`}
+        className={`${fontGeistSans.variable} ${fontGeistMono.variable} ${fontZain.variable} ${fontPoiretOne.variable} antialiased`}
       >
         <ClientWrapper>{children}</ClientWrapper>
       </body>
