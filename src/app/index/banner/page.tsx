1ed01eefa6f74403a3bb1707e601d8cc2b2e8722
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-21 00:14:37 +0530

    refactor (index::banner): v2 - update banner comp

--- src/app/index/banner/page.tsx
+++ src/app/index/banner/page.tsx
@@ -1,31 +1,39 @@
+"use client";
+
 import Image from "next/image";
-import BannerImage from "../../../../public/images/banner.png";
+import { FaArrowRight } from "react-icons/fa6";
+import { StyledButtonPrimary } from "@/components/buttons/styled-button-primary";
+import BannerMicImage from "../../../../public/assets/images/banner-mic.png";
 
 const Banner = () => {
   return (
-    <div className="px-[8%] lg:px-[16%] py-20 light-section">
+    <div className="light-section px-[8%] lg:px-[12%] py-20">
       <div className="banner bg-banner-background px-[7%] rounded-xl">
         <div className="">
           <div className="flex flex-col lg:flex-row items-center gap-10 lg:gap-25">
             <div className="w-full lg:w-1/2 relative">
-              <h1 className="text-4xl lg:text-5x1 text-text font-semibold my-10">
+              <h1 className="text-4xl lg:text-5xl text-dark-text leading-[1.1] font-semibold my-10">
                 Get the Latest Episode & Never Miss an Episode
               </h1>
               <div
                 className="flex flex-col md:flex-row items-start justify-between 
-                md:items-center gap-5 md:gap-0 bg-black rounded-2xl md:rounded-full p-5 "
+                md:items-center gap-5 md:gap-0 bg-black rounded-2xl md:rounded-full px-3 py-2 mb-4"
               >
                 <div className="email-input">
                   <input
                     type="text"
                     placeholder="Enter Email"
-                    className=" text-white px-3 py-2 outline-none"
+                    className="text-white placeholder:text-gray-300 px-3 py-2 outline-none"
                   />
                 </div>
                 <div className="sub-btn">
-                  <button type="button" className="btn btn1">
-                    Subscribe <i className="bi bi-arrow-right-short"></i>
-                  </button>
+                  <StyledButtonPrimary
+                    onClick={() => {}}
+                    className="tracking-wide"
+                    icon={<FaArrowRight size={12} />}
+                  >
+                    Subscribe
+                  </StyledButtonPrimary>
                 </div>
               </div>
             </div>
@@ -33,7 +41,7 @@ const Banner = () => {
             <div className="w-full lg:w-1/2">
               <div className="banner-img">
                 <Image
-                  src={BannerImage}
+                  src={BannerMicImage}
                   alt="bannerImg"
                   className="w-full h-full object-cover"
                 />
