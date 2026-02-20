312263e1bf787f1f7211d09f547d39b645b0262a
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-20 17:19:17 +0530

    refactor (index::why-choose-us): v1 - update why-choose-us comp

--- src/app/index/why-choose-us/page.tsx
+++ src/app/index/why-choose-us/page.tsx
@@ -2,13 +2,14 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import HeaderTitle from "@/components/header-title";
 import WhyChooseUsImage1 from "../../../../public/images/why-choose-1.png";
 import WhyChooseUsImage2 from "../../../../public/images/why-choose-2.png";
 import WhyChooseUsImage3 from "../../../../public/images/why-choose-3.png";
 
 const WhyChooseUs = () => {
   return (
-    <div className="light-section bg-black/80">
+    <div className="light-section">
       <div className="px-[8%] lg:px-[16%] py-30">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-20">
           <div className="w-full lg:w-1/2">
@@ -29,17 +30,13 @@ const WhyChooseUs = () => {
 
           <div className="w-full lg:w-1/2">
             <div className="content">
-              <div className="title flex flex-col gap-2">
-                <div>
-                  <h2 className="inline-block px-4 py-2 rounded-full text-primary text-2xl font-normal border border-primary">
-                    <i className="bi bi-rocket-takeoff pe-4"></i>
-                    Why Choose Us
-                  </h2>
-                </div>
-                <h1 className="text-5xl text-white lg:text-6xl font-semibold mt-7">
-                  What Makes Us Different From Other
-                </h1>
-              </div>
+              {/* header title */}
+              <HeaderTitle
+                badgeText="Why Choose Us"
+                badgeIcon="bi bi-rocket-takeoff"
+                title="What Makes Us Different From Other"
+              />
+
               <p className="my-5 tracking-wider text-white">
                 Explore vibrant soundscapes where stories of every kind come
                 alive, taking you on an immersive journey and through
