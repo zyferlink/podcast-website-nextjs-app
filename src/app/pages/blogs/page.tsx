7f2640868b3500b89fbf569fae218ba8207b385b
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-21 12:13:50 +0530

    refactor (pages::blogs): v3 - update blogs comp

--- src/app/pages/blogs/page.tsx
+++ src/app/pages/blogs/page.tsx
@@ -2,44 +2,17 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import BrandIcon1 from "../../../../public/images/brand-icon-1.png";
-import BrandIcon2 from "../../../../public/images/brand-icon-2.png";
-import BrandIcon3 from "../../../../public/images/brand-icon-3.png";
-import BrandIcon4 from "../../../../public/images/brand-icon-4.png";
-import BrandIcon5 from "../../../../public/images/brand-icon-5.png";
+import PageHeader from "@/components/page-header/page-header";
 import PageBanner1 from "../../../../public/images/Page-banner-1.png";
 import PageBanner2 from "../../../../public/images/Page-banner-2.png";
-import PageElement1 from "../../../../public/images/page-elm-1.png";
-import PageElement2 from "../../../../public/images/page-elm-2.png";
-import PageElement3 from "../../../../public/images/page-elm-3.png";
-import PageElement4 from "../../../../public/images/page-elm-4.png";
 import RocketIcon from "../../../../public/images/rocket-icon.png";
 import BlogsData from "../../../data/BlogsData.json";
 
 const Blog = () => {
   return (
     <>
-      {/* Page Section */}
-      <div className="page-section bg-black/98 text-white">
-        <Image src={PageElement2} alt="Element" className="elm2 elemet" />
-        <Image src={PageElement3} alt="Element" className="elm3 elemet" />
-        <Image src={PageElement4} alt="Element" className="elm4 elemet" />
-        <div className="page-content w-full md:w-1/2 flex justify-center flex-col pt-10">
-          <Image src={PageElement1} alt="Element" className="w-full h-full" />
-
-          <h1 className="text-5xl lg:text-6xl justify-center my-6">
-            Our Recent <span className="text-primary">Articles</span>
-          </h1>
-
-          <div className="flex items-center justify-center gap-3 cursor-pointer mt-5">
-            <Image src={BrandIcon1} alt="brand" />
-            <Image src={BrandIcon2} alt="brand" />
-            <Image src={BrandIcon3} alt="brand" />
-            <Image src={BrandIcon4} alt="brand" />
-            <Image src={BrandIcon5} alt="brand" />
-          </div>
-        </div>
-      </div>
+      {/* Page Header */}
+      <PageHeader title="Our Recent Articles" />
 
       <div className="px-[8%] lg:px-[16%] py-30 pb-20 bg-black/98 text-white">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
