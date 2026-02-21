22a222d8eab5ad98626b070b7204661b5f639e49
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-21 13:27:52 +0530

    refactor (pages::about): v1 - update about comp

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -6,6 +6,7 @@ import CountUp from "react-countup";
 import Banner from "@/app/index/banner/page";
 import HostProfiles from "@/app/index/host-profiles/page";
 import Testimonial from "@/app/index/testimonial/page";
+import PageHeader from "@/components/page-header/page-header";
 import BrandIcon1 from "../../../../public/images/brand-icon-1.png";
 import BrandIcon2 from "../../../../public/images/brand-icon-2.png";
 import BrandIcon3 from "../../../../public/images/brand-icon-3.png";
@@ -26,27 +27,8 @@ import WhyChooseUsImage3 from "../../../../public/images/why-choose-3.png";
 const About = () => {
   return (
     <>
-      {/* Page Section */}
-      <div className="page-section bg-black/98 text-white">
-        <Image src={PageElement2} alt="Element" className="elm2 elemet" />
-        <Image src={PageElement3} alt="Element" className="elm3 elemet" />
-        <Image src={PageElement4} alt="Element" className="elm4 elemet" />
-        <div className="page-content w-full md:w-1/2 flex justify-center flex-col pt-10">
-          <Image src={PageElement1} alt="Element" className="w-full h-full" />
-
-          <h1 className="text-6xl lg:text-8x1 justify-center my-6">
-            About <span className="text-primary">VoiceFlow</span>
-          </h1>
-
-          <div className="flex items-center justify-center gap-3 cursor-pointer mt-5">
-            <Image src={BrandIcon1} alt="brand" />
-            <Image src={BrandIcon2} alt="brand" />
-            <Image src={BrandIcon3} alt="brand" />
-            <Image src={BrandIcon4} alt="brand" />
-            <Image src={BrandIcon5} alt="brand" />
-          </div>
-        </div>
-      </div>
+      {/* Page Header */}
+      <PageHeader title="About VoiceFlow" />
 
       {/* Experience */}
       <div className="dark-section pb-20 bg-black/98 text-white">
