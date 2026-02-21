32ac46b36df6150ddca1318ac846b9898cec5e35
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-21 09:52:31 +0530

    refactor (pages::episodes): v16 - update episodes comp

--- src/app/pages/episodes/page.tsx
+++ src/app/pages/episodes/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { FaArrowRight } from "react-icons/fa6";
 import { StyledButtonSecondary } from "@/components/buttons/styled-button-secondary";
 import EpisodeCard from "@/components/episode-card";
+import PageBannerCustom from "@/components/page-banner-custom";
 import PageHeader from "@/components/page-header/page-header";
 import { type Episode, EpisodeData } from "@/data/episodes";
-import PageBanner1 from "../../../../public/images/Page-banner-1.png";
-import PageBanner2 from "../../../../public/images/Page-banner-2.png";
-import RocketIcon from "../../../../public/images/rocket-icon.png";
 
 const Episodes = () => {
   const [showAll, setShowAll] = useState(false);
@@ -153,57 +149,18 @@ const Episodes = () => {
 
           {EpisodeData.length > 10 && (
             <div className="flex justify-center mt-12">
-              <button
-                type="button"
-                className="btn btn2"
+              <StyledButtonSecondary
                 onClick={() => setShowAll(!showAll)}
+                className="px-8 text-xl tracking-normal"
+                icon={<FaArrowRight size={12} />}
               >
                 {showAll ? "Show less" : "Show More"}
-                <i className="bi bi-arrow-right-short"></i>
-              </button>
+              </StyledButtonSecondary>
             </div>
           )}
         </div>
 
-        <div className="px-[8%] lg:px-[10%] py-20">
-          <div className="page-banner p-15 flex flex-col justify-center items-center text-center rounded-2xl relative">
-            <Image
-              src={PageBanner1}
-              alt="PageBanner1"
-              width={500}
-              height={550}
-              className="hidden lg:block absolute bottom-0 left-0"
-            />
-            <Image
-              src={PageBanner2}
-              alt="PageBanner2"
-              width={500}
-              height={550}
-              className="hidden lg:block absolute bottom-0 right-0"
-            />
-
-            <div className="music-waves"></div>
-            <div className="my-3 mt-5">
-              <span className="flex items-center gap-2 text-black px-4 py-3 rounded-full border border-black">
-                <Image
-                  src={RocketIcon}
-                  alt="rocketIcon"
-                  width={30}
-                  height={30}
-                />
-                <h2 className="text-xl">Call To Action</h2>
-              </span>
-            </div>
-            <h1 className="text-2xl lg:text-3xl mb-5 font-semibold w-full lg:w-[50%] text-text">
-              Let's Discuss <br />
-              For <br />
-              Any Episodes
-            </h1>
-            <button type="button" className="btn btn2 bg-text">
-              Get In Touch <i className="bi bi-arrow-right-short"></i>
-            </button>
-          </div>
-        </div>
+        <PageBannerCustom />
       </div>
     </>
   );
