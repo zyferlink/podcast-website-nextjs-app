af74fd6d63054943d8e8a1c5db6db68603775f76
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-21 10:08:22 +0530

    refactor (pages::favorite-episodes): v1 - update favorite-episodes comp

--- src/app/pages/episodes/favorite-episodes/page.tsx
+++ src/app/pages/episodes/favorite-episodes/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import PageHeader from "@/components/page-header/page-header";
 import BrandIcon1 from "../../../../../public/images/brand-icon-1.png";
 import BrandIcon2 from "../../../../../public/images/brand-icon-2.png";
 import BrandIcon3 from "../../../../../public/images/brand-icon-3.png";
@@ -50,27 +51,8 @@ const FavoriteEpisodes = () => {
 
   return (
     <>
-      {/* Page Section */}
-      <div className="page-section bg-black/98 text-white">
-        <Image src={PageElement2} alt="Element" className="elm2 elemet" />
-        <Image src={PageElement3} alt="Element" className="elm3 elemet" />
-        <Image src={PageElement4} alt="Element" className="elm4 elemet" />
-        <div className="page-content w-full md:w-1/2 flex justify-center flex-col pt-10">
-          <Image src={PageElement1} alt="Element" className="w-full h-full" />
-
-          <h1 className="text-6xl lg:text-8x1 justify-center my-6">
-            Favorite <span className="text-primary">Episodes</span>
-          </h1>
-
-          <div className="flex items-center justify-center gap-3 cursor-pointer mt-5">
-            <Image src={BrandIcon1} alt="brand" />
-            <Image src={BrandIcon2} alt="brand" />
-            <Image src={BrandIcon3} alt="brand" />
-            <Image src={BrandIcon4} alt="brand" />
-            <Image src={BrandIcon5} alt="brand" />
-          </div>
-        </div>
-      </div>
+      {/* Page Header */}
+      <PageHeader title="Favorite Episodes" />
 
       {/* Favorite Episodes */}
       <div className="dark-section bg-black/98 text-white px-[8%] lg:px-[16%] py-15">
